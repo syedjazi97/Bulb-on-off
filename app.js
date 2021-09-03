6649a428273045ea81d3ef42c48b9ac46747cd16
syedjazi97 <syedjazi97@gmail.com>
2021-09-03 04:46:53 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function onBulb(){
+    var bulb = document.getElementById('bulb');
+    bulb .src= 'on.jpg'
+}
+
+function offBulb(){
+    var bulb = document.getElementById('bulb');
+    bulb .src= 'off.jpg'
+}
